bb611480aebc5730694a9e93d9c5ec703f5fdfff
Zihao Chen <chen.zihao@microsoft.com>
2019-03-26 13:57:04 +0800

    Add RestServer and RestClient

---
+++ HighAvailabilityModule.Client.Rest/RestMembershipClient.cs
@@ -0,0 +1,41 @@
+﻿namespace HighAvailabilityModule.Client.Rest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using HighAvailabilityModule.Interface;
+
+    public class RestMembershipClient : IMembershipClient
+    {
+        private RestClientImpl impl;
+
+        private HttpClient httpClient;
+
+        public RestMembershipClient(TimeSpan operationTimeout)
+        {
+            this.httpClient = new HttpClient { Timeout = operationTimeout };
+            this.impl = new RestClientImpl(this.httpClient);
+        }
+
+        public string BaseUri
+        {
+            get => this.impl.BaseUrl;
+            set => this.impl.BaseUrl = value;
+        }
+
+        public Task HeartBeatAsync(string uuid, HeartBeatEntry lastSeenEntry) => this.impl.HeartBeatAsync(uuid, lastSeenEntry);
+
+        public Task<HeartBeatEntry> GetHeartBeatEntryAsync() => this.impl.GetHeartBeatEntryAsync();
+
+        public string GenerateUuid() => Guid.NewGuid().ToString();
+
+        public TimeSpan OperationTimeout
+        {
+            get => this.httpClient.Timeout;
+            set => this.httpClient.Timeout = value;
+        }
+    }
+}
